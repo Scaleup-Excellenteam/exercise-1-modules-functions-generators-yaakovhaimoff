15abaf9e7b318b6c994c980482357135f661bfc0
Yaakov Haimoff <hyaakov@aol.com>
2023-03-18 20:43:53 +0200

    added peace of cake

---
+++ 5.2_peace_of_cake.py
@@ -0,0 +1,19 @@
+def get_recipe_price(prices, optionals, *ingredients):
+    # Loop through the items (key-value pairs) of the dictionary
+    price, i = 0, 0
+    for key, value in prices.items():
+        if key in optionals:
+            continue
+        else:
+            price += value * ingredients[i]
+        i += 1
+    print(int(price/100))
+
+
+optionals = []
+chocolate, milk = 200, 100
+get_recipe_price({'chocolate': 18, 'milk': 8}, optionals, chocolate, milk)
+optionals = ['milk']
+chocolate = 300
+get_recipe_price({'chocolate': 18, 'milk': 8}, optionals, chocolate)
+get_recipe_price({}, [])
