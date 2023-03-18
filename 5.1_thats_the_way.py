499c7bfac72106ae4794d2af070a62464a6a8c17
Yaakov Haimoff <hyaakov@aol.com>
2023-03-18 20:26:15 +0200

    finished thats the way

--- 5.1_thats_the_way.py
+++ 5.1_thats_the_way.py
@@ -1,11 +1,16 @@
 import os
+import re
 
-folder_path = '/path/to/folder'  # replace with the path to your folder
-search_pattern = 'pattern'  # replace with the pattern you want to search for
 
-# Loop over all files in the folder
-for filename in os.listdir(folder_path):
-    # Check if the file name contains the search pattern
-    if search_pattern in filename:
-        # If the pattern is found, print the file name
-        print(filename)
+def that_the_way(folder_path):
+    # Loop over all files in the folder
+    for filename in os.listdir(folder_path):
+        # Check if the file name contains the search pattern
+        if re.match(search_pattern, filename): # If the pattern is found, print the file name
+            print(filename)
+
+
+folder_path = 'images'  # replace with the path to your folder
+search_pattern = r'^deep.*'
+that_the_way(folder_path)
+
